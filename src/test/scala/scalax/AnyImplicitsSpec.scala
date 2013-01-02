5af0ca5bc7f68fe2cdd6e2250ed9e6c63aae4832
Yaroslav Klymko <t3hnar@gmail.com>
2013-01-02 21:34:14 +0200

    Add AnyImplicitsSpec

---
+++ src/test/scala/scalax/AnyImplicitsSpec.scala
@@ -0,0 +1,33 @@
+package scalax
+
+import org.specs2.mutable.SpecificationWithJUnit
+
+/**
+ * @author Yaroslav Klymko
+ */
+class AnyImplicitsSpec extends SpecificationWithJUnit {
+  "AnyImplicitsSpec" should {
+    "asInstanceOfOpt" in {
+      (null: Any).asInstanceOfOpt[String] must beNone
+
+      ("string": Any).asInstanceOfOpt[String] must beSome
+      ("string": Any).asInstanceOfOpt[Int] must beNone
+
+      (new A {}: Any).asInstanceOfOpt[A] must beSome
+      (new A {}: Any).asInstanceOfOpt[B] must beNone
+
+      (new A with B {}: Any).asInstanceOfOpt[A] must beSome
+      (new A with B {}: Any).asInstanceOfOpt[B] must beSome
+      (new A with B {}: Any).asInstanceOfOpt[A] must beSome
+      (new A with B {}: Any).asInstanceOfOpt[C] must beNone
+
+      (new C {}: Any).asInstanceOfOpt[A] must beSome
+      (new C {}: Any).asInstanceOfOpt[B] must beSome
+      (new C {}: Any).asInstanceOfOpt[Int] must beNone
+    }
+  }
+
+  trait A
+  trait B
+  trait C extends A with B
+}
