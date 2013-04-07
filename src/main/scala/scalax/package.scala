e38b5f0c1773d8c14af4cc14cb5132572d6c6ef9
Yaroslav Klymko <t3hnar@gmail.com>
2013-04-07 12:43:05 +0300

    add asInstanceOfOptUnsafe

--- src/main/scala/scalax/package.scala
+++ src/main/scala/scalax/package.scala
@@ -6,6 +6,12 @@ import reflect.ClassTag
 package object scalax {
 
   implicit class RichAny[A](val self: A) extends AnyVal {
-    def asInstanceOfOpt[T <: A](implicit tag: ClassTag[T]): Option[T] = tag.unapply(self)
+    def asInstanceOfOpt[T <: A](implicit tag: ClassTag[T]): Option[T] = asInstanceOfOptUnsafe[T]
+
+    /**
+     *  "".asInstanceOfOptUnsafe[Int] compiles
+     *  "".asInstanceOfOpt[Int] does not compile
+     */
+    def asInstanceOfOptUnsafe[T](implicit tag: ClassTag[T]): Option[T] = tag.unapply(self)
   }
 }
