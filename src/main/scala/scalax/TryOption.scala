e56652a2da918ec84f52c314464e81da43f6fa65
Yaroslav Klymko <t3hnar@gmail.com>
2013-07-29 11:14:00 +0300

    fix compile warning

--- src/main/scala/scalax/TryOption.scala
+++ src/main/scala/scalax/TryOption.scala
@@ -5,7 +5,7 @@ import scala.util.Try
 /**
  * @author Yaroslav Klymko
  */
-@deprecated("Use scala.util.Try instead")
+@deprecated("Use scala.util.Try instead", "1.1")
 object TryOption {
   def apply[T](body: => T): Option[T] = Try(body).toOption
 }
