d1c3a98bf3e6aaceb368a51b08b454e9e1ae3006
Yaroslav Klymko <t3hnar@gmail.com>
2013-11-07 11:52:53 +0200

    fix tests

--- src/test/scala/scalax/RichDurationSpec.scala
+++ src/test/scala/scalax/RichDurationSpec.scala
@@ -14,24 +14,23 @@ class RichDurationSpec extends SpecificationWithJUnit with NoTimeConversions {
       Duration.Inf.toCoarsest mustEqual Duration.Inf
       Duration.MinusInf.toCoarsest mustEqual Duration.MinusInf
     }
-    "convert finite durations" in {
-      List(
-        (60 minutes, 1 hour),
-        (2000 millis, 2 seconds),
-        (2000 micros, 2 millis),
-        (2000 nanos, 2 micros),
-        (2000000 nanos, 2 millis),
-        (48 hours, 2 days),
-        (5 seconds, 5 seconds),
-        (0 seconds, 0 seconds),
-        (0 days, 0 days),
-        (1 days, 1 days)
-      ) foreach {
-        case (x, expected) =>
-          val actual = x.toCoarsest
-          assert(actual.unit == expected.unit)
-          assert(actual.length == expected.length)
-      }
+
+    "convert finite durations" in forall(List(
+      (60 minutes, 1 hour),
+      (2000 millis, 2 seconds),
+      (2000 micros, 2 millis),
+      (2000 nanos, 2 micros),
+      (2000000 nanos, 2 millis),
+      (48 hours, 2 days),
+      (5 seconds, 5 seconds),
+      (0 seconds, 0 seconds),
+      (0 days, 0 days),
+      (1 days, 1 days)
+    )) {
+      case (x, expected) =>
+        val actual = x.toCoarsest
+        actual.unit mustEqual expected.unit
+        actual.length mustEqual expected.length
     }
   }
 }
