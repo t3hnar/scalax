9db0add90fd2b26a0e41e20d3f7ea1218813b5ab
Yaroslav Klymko <t3hnar@gmail.com>
2015-04-08 10:07:51 +0300

    fix RichDurationSpec

--- src/test/scala/com/github/t3hnar/scalax/RichDurationSpec.scala
+++ src/test/scala/com/github/t3hnar/scalax/RichDurationSpec.scala
@@ -10,23 +10,23 @@ import scala.concurrent.duration._
 class RichDurationSpec extends Specification with NoTimeConversions {
   "RichDuration.toCoarsest" should {
     "convert infinite to infinite" in {
-      Duration.Inf.toCoarsest mustEqual Duration.Inf
-      Duration.MinusInf.toCoarsest mustEqual Duration.MinusInf
+      RichDuration(Duration.Inf).toCoarsest mustEqual Duration.Inf
+      RichDuration(Duration.MinusInf).toCoarsest mustEqual Duration.MinusInf
     }
 
     "convert finite durations" in forall(List(
-      (60 minutes, 1 hour),
-      (2000 millis, 2 seconds),
-      (2000 micros, 2 millis),
-      (2000 nanos, 2 micros),
-      (2000000 nanos, 2 millis),
-      (48 hours, 2 days),
-      (5 seconds, 5 seconds),
-      (0 seconds, 0 seconds),
-      (0 days, 0 days),
-      (1 days, 1 days))) {
+      (60.minutes, 1.hour),
+      (2000.millis, 2.seconds),
+      (2000.micros, 2.millis),
+      (2000.nanos, 2.micros),
+      (2000000.nanos, 2.millis),
+      (48.hours, 2.days),
+      (5.seconds, 5.seconds),
+      (0.seconds, 0.seconds),
+      (0.days, 0.days),
+      (1.days, 1.days))) {
       case (x, expected) =>
-        val actual = x.toCoarsest
+        val actual = RichDuration(x).toCoarsest
         actual.unit mustEqual expected.unit
         actual.length mustEqual expected.length
     }
